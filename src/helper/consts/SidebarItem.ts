01ba3a2d53ff4de79192e22fd89f1e613f7acad2
DatWithTheWorld <nguyenducdat592004@gmail.com>
2025-01-10 10:56:10 +0700

    #1 Build src base

---
+++ src/helper/consts/SidebarItem.ts
@@ -0,0 +1,10 @@
+import { SidebarItem } from "../../Interface/SidebarItem";
+
+const sidebarItems: SidebarItem[] = [
+    {
+        name: 'Guests',
+        icons:
+    }
+]
+
+export default sidebarItems;
